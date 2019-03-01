2a4c5aff0f95e17d3e2c4b8855212da371164963
projektorius96 <34185919+projektorius96@users.noreply.github.com>
2019-03-01 02:11:56 +0200

    Delete eListener-with-multiParameters.js

--- Events/eListener-with-multiParameters.js
+++
@@ -1,8 +0,0 @@
-function eHandler(e, addCustomColor) {
-var objTarget = e.target;
-objTarget.style.backgroundColor = addCustomColor;}
-
-var variableWin = this; // keyword THIS represent [object Window] in JS
-
-variableWin.addEventListener('mouseover', function (e) { 
-eHandler (e, 'yellow'); }, false);
