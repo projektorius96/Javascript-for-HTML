65ac61764e1f4ed4e0b459f15fb14dc4ab797f23
projektorius96 <34185919+projektorius96@users.noreply.github.com>
2019-09-29 00:06:59 +0300

    Update showMousePos(consoleFnOnly).js

--- Events/MouseEvents/showMousePos(consoleFnOnly).js
+++ Events/MouseEvents/showMousePos(consoleFnOnly).js
@@ -1,11 +1,7 @@
 function showPosition(e) {
   e.value = console.log('X coordinate: ' + e.pageX); // cx.value <= eventRegisteredData [X]
   e.value = console.log('Y coordinate: ' + e.pageY); // cy.value <= eventRegisteredData [Y]
-}
-
-function showPosition(e) {
-  e.value = console.log('X coordinate: ' + e.pageX); // cx.value <= eventRegisteredData [X]
-  e.value = console.log('Y coordinate: ' + e.pageY); // cy.value <= eventRegisteredData [Y]
+if (e.pageX, e.pageY) {alert("Open console to see coords!")}
 }
 document.body.addEventListener('click', function (e) { 
 showPosition (e); }, false);
