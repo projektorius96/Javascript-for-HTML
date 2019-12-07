e2ee9018f7d75a61a4328ff15ee8158c87d68c51
projektorius96 <34185919+projektorius96@users.noreply.github.com>
2019-12-07 23:38:31 +0200

    Update and rename get-NodeList.js to get-NodeList(explained).js

--- domHTML/NodeList-object/get-NodeList(explained).js
+++ domHTML/NodeList-object/get-NodeList(explained).js
@@ -18,21 +18,19 @@ and choose a NodeList as array-like object to work with and apply DOM methods to
 // STEP 1: Iterate over
 
 // Let's say we uploaded .html file with code above, and we have our browser's console window opened:
-/*
+
 var checkAnyNodes = document.querySelectorAll("UL"); // getting reference of any NodeList we have in our .html file presented
 checkAnyNodes.toLocaleString(); // double check if it's [object NodeList] we are working with
 checkAnyNodes.forEach((index, item) => {console.log(index, item)}); // iterate over the NodeList we have
-// Console output of the above code: <ul>...</ul> as iteration result we get 0, hereby 0 means there are only one unordered list with listed items within
-*/
+// Console output of the above code: <ul>...</ul> as iteration result we get 0, hereby 0 means there are only one unordered list with listed items within.
 
-// STEP 2: Get different way to access array-like object in order to modify it later on if needed.
+// STEP 2.1: Get different ways to access array-like object in order to modify it later on if needed.
 
 checkAnyNodes[0].innerHTML
 /* We get all stuff which is inside <UL> including white space. It's important to consider white space whilst working with NodeList
 because white space is also indexed and if once you will need to get Element_NODE, but you choose wrong index you probably would get white space instead
 which is sometimes even not displayed. By user, that probably would be considered as error. Ususally it is just a misacknowledgment.
-*/ 
-
+*/
 
 checkAnyNodes[0].children
 /*
@@ -49,3 +47,13 @@ checkAnyNodes[0].childNodes.item(0).textContent
 // In order to get rid of ["↵    "] withing html content we should define content itself in sementically minified in-line way withoud ENTER keys pressed
 // More information about ["↵    "]: https://stackoverflow.com/questions/21572938/what-is-the-character-in-chrome-console
 // BEWARE: Comment IT IS NOT A WHITESPACE itself. IF COMMENT would be practically removed, but theoretically would stay in our mind that could be considered as whitespace
+
+
+// STEP 2.2: Get different ways to access array-like object in order to modify it later on if needed.
+
+var checkAnyParentNodes = document.body.parentNode;
+// __proto__: HTMLHtmlElement
+
+checkAnyParentNodes.innerHTML;
+// Console output: we get all static HTML mirror.
+
