9a33ce44f4dfbe2b11b413ce9820570227eea4a4
projektorius96 <34185919+projektorius96@users.noreply.github.com>
2019-12-07 11:42:20 +0200

    Update get-NodeList.js

--- domHTML/NodeList-object/get-NodeList.js
+++ domHTML/NodeList-object/get-NodeList.js
@@ -15,6 +15,8 @@ and choose a NodeList as array-like object to work with and apply DOM methods to
 </ul>
 </body>
 
+// STEP 1: Iterate over
+
 */
 
 // Let's say we uploaded .html file with code above, and we have our browser's console window opened:
@@ -23,4 +25,19 @@ var checkAnyNodes = document.querySelectorAll("UL"); // getting reference of any
 checkAnyNodes.toLocaleString(); // double check if it's [object NodeList] we are working with
 checkAnyNodes.forEach((index, item) => {console.log(index, item)}); // iterate over the NodeList we have
 // Console output of the above code: <ul>...</ul> as iteration result we get 0, hereby 0 means there are only one unordered list with listed items within
+*/
+
+// STEP 2: Get different way to access array-like object in order to modify it later on
+
+
+/*
+checkAnyNodes[0].innerHTML
+/* We get all stuff which is inside <UL> including white space. It's important to consider white space whilst working with NodeList
+because white space is also indexed and if once you will need to get Element_NODE, but you choose wrong index you probably would get white space instead
+which is sometimes even not displayed. By user, that probably would be considered as error. Ususally it is just a misacknowledgment.
+*/ 
+*/
+/*
+checkAnyNodes[0].childNodes
+// Console output: NodeList(15) [text, comment, text, li, text, comment, text, li, text, comment, text, li, text, comment, text]
 */
