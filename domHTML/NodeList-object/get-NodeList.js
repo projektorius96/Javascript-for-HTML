e4565dc9f46dce1fe337494eeaf649e16a236338
projektorius96 <34185919+projektorius96@users.noreply.github.com>
2019-12-07 12:08:59 +0200

    Update get-NodeList.js

--- domHTML/NodeList-object/get-NodeList.js
+++ domHTML/NodeList-object/get-NodeList.js
@@ -2,22 +2,20 @@
 // We have two options as to work with: HtmlCollection or NodeList
 /* Because it's small educational project and .html file content displayed below has whitespace as COMMENT_NODE we have to be broad minded
 and choose a NodeList as array-like object to work with and apply DOM methods to it as more likely [native-to-DOM] code itself */
-/*
+
 <body>
-<ul>
-    <!-- whitespace specifically comment text node -->
+<ul><!-- REMOVE THIS COMMENT TO BE A WHITESPACE MADE BY ENTER KEY -->
+    <!-- comment #text node -->
   <li>firstElementChild[0Index]</li>
-    <!-- whitespace specifically comment text node -->
+    <!-- comment #text node -->
   <li>secondElementChild[1Index]</li>
-    <!-- whitespace specifically comment text node -->
+    <!-- comment #text node -->
   <li>thirdElementChild[2Index]</li>
-    <!-- whitespace specifically comment text node -->
+    <!-- comment #text node -->
 </ul>
 </body>
 
 // STEP 1: Iterate over
-
-*/
 
 // Let's say we uploaded .html file with code above, and we have our browser's console window opened:
 /*
@@ -27,16 +25,27 @@ checkAnyNodes.forEach((index, item) => {console.log(index, item)}); // iterate o
 // Console output of the above code: <ul>...</ul> as iteration result we get 0, hereby 0 means there are only one unordered list with listed items within
 */
 
-// STEP 2: Get different way to access array-like object in order to modify it later on
+// STEP 2: Get different way to access array-like object in order to modify it later on if needed.
 
-
-/*
 checkAnyNodes[0].innerHTML
 /* We get all stuff which is inside <UL> including white space. It's important to consider white space whilst working with NodeList
 because white space is also indexed and if once you will need to get Element_NODE, but you choose wrong index you probably would get white space instead
 which is sometimes even not displayed. By user, that probably would be considered as error. Ususally it is just a misacknowledgment.
 */ 
+
+
+checkAnyNodes[0].children
+/*
+// Console output: (3) [li, li, li]
+// It's not a part of NodeList we are currently working with but HtmlCollection property
 */
+
+checkAnyNodes[0].childNodes.item(0).textContent
+// Console output: "↵    " . Be noticed that ["↵    "] is considered as [text] inside .html BUT ACTUALLY it's a new line made by ENTER key
+// In order to get rid of ["↵    "] withing html content we should define content itself in sementically minified in-line way withoud ENTER keys pressed
+// More information about ["↵    "]: https://stackoverflow.com/questions/21572938/what-is-the-character-in-chrome-console
+// BEWARE: Comment IT IS NOT A WHITESPACE itself. IF COMMENT would be practically removed, but theoretically would stay in our mind that could be considered as whitespace
+
 /*
 checkAnyNodes[0].childNodes
 // Console output: NodeList(15) [text, comment, text, li, text, comment, text, li, text, comment, text, li, text, comment, text]
