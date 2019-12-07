ba0b6d91771fd0355f6022b41d374adc371b015e
projektorius96 <34185919+projektorius96@users.noreply.github.com>
2019-12-07 11:20:43 +0200

    Update get-NodeList.js

--- domHTML/NodeList-object/get-NodeList.js
+++ domHTML/NodeList-object/get-NodeList.js
@@ -1,6 +1,7 @@
 // Imagine we have statically created HTML content:
 // We have two options as to work with: HtmlCollection or NodeList
-// Beca
+/* Because it's small educational project and .html file content displayed below has whitespace as COMMENT_NODE we have to be broad minded
+and choose a NodeList as array-like object to work with and apply DOM methods to it as more likely [native-to-DOM] code itself */
 /*
 <body>
 <ul>
@@ -18,8 +19,8 @@
 
 // Let's say we uploaded .html file with code above, and we have our browser's console window opened:
 /*
-var checkAnyNodes = document.querySelectorAll("UL");
+var checkAnyNodes = document.querySelectorAll("UL"); // getting reference of any NodeList we have in our .html file presented
 checkAnyNodes.toLocaleString(); // double check if it's [object NodeList] we are working with
-checkAnyNodes.forEach((index, item) => {console.log(index, item)}); // iterate over NodeList
+checkAnyNodes.forEach((index, item) => {console.log(index, item)}); // iterate over the NodeList we have
 // Console output of the above code: <ul>...</ul> as iteration result we get 0, hereby 0 means there are only one unordered list with listed items within
 */
