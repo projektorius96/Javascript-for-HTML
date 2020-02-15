b93c6da451f0ec75d1fc071e458db6559d128b8b
projektorius96 <34185919+projektorius96@users.noreply.github.com>
2020-02-15 17:57:17 +0200

    Update parentNode.insertBefore(para1, para2).js

--- domHTML/parentNode.insertBefore(para1, para2).js
+++ domHTML/parentNode.insertBefore(para1, para2).js
@@ -1,10 +1,20 @@
 var refNode = document.querySelectorAll("*");
 // refNode as NodeList: [html, head, body]
-var currentNode = document.createElement("FOOTER");
-// ...
-refNode[2].appendChild(currentNode);
-// [html, head, body], childNodes of <body>: <body><header><article></article></header></body>
-var beforeThisNode = document.createElement("HEADER");
+var beforeThisNode = document.createElement("FOOTER");
+// beforeThisNode is not appended yet
+refNode[2].appendChild(beforeThisNode);
+// [html, head, body, ..., footer]
 
-let insertedNode = refNode[2].insertBefore(beforeThisNode, currentNode);
-// refNode: [html, head, body: [header, footer]]
+var currentNodeToBeAdded = document.createElement("HEADER");
+// currentNodeToBeAdded is not be added yet
+
+let LetsInsertBefore = refNode[2].insertBefore(currentNodeToBeAdded, beforeThisNode); // [html, head, body, HEADER, footer]
+
+// let LetsInsertAFTER = refNode[2].insertBefore(currentNodeToBeAdded, beforeThisNode.nextSibling); // [html, head, body, FOOTER, header]
+
+// ... in order to visualize new nodeList we need to refresh currentNodeList i. e. static refNode
+// unfortunately, document.querySelectorAll returns static NodeList!
+var refNodeRefreshed = document.querySelectorAll("*"); // this line refreshes currentNode i.e. refNode
+// refNode just have been refreshed
+refNodeRefreshed
+// NodeList(5) [html, head, body, header, footer]
