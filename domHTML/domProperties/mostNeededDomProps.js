117261aa9114257c39c4637cc36673607bbb0e76
projektorius96 <34185919+projektorius96@users.noreply.github.com>
2020-02-15 18:07:28 +0200

    Create mostNeededDomProps.js

---
+++ domHTML/domProperties/mostNeededDomProps.js
@@ -0,0 +1,3 @@
+document.documentElement.parentNode.children // HTMLCollection [html]
+
+document.documentElement.parentNode.children[0].appendChild(newNode); // wheare as newNode is some user-custom created Node as HTMLElement
