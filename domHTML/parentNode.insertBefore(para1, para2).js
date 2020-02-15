477be0dd08bbee2ed63148682483290be7b0766c
projektorius96 <34185919+projektorius96@users.noreply.github.com>
2020-02-15 18:22:25 +0200

    Update parentNode.insertBefore(para1, para2).js

--- domHTML/parentNode.insertBefore(para1, para2).js
+++ domHTML/parentNode.insertBefore(para1, para2).js
@@ -17,4 +17,13 @@ let LetsInsertBefore = refNode[2].insertBefore(currentNodeToBeAdded, beforeThisN
 var refNodeRefreshed = document.querySelectorAll("*"); // this line refreshes currentNode i.e. refNode
 // refNode just have been refreshed
 refNodeRefreshed
-// NodeList(5) [html, head, body, header, footer]
+// NodeList(5) [html, head, body, header, footer] only in NodeList sorted way. HOWEVER (SEE NEXT LINE)
+// document.documentElement.firstElementChild.nextElementSibling.children shows...
+// ...that in hierarchical HTML way footer is a child of BODY, while HEADER IS NOT a child of BODY
+
+/*
+// document.documentElement === html && document.documentElement.parentNode === document
+document.documentElement.parentNode.children[0].appendChild(LetsInsertBefore);
+document.body.childNodes[0].innerHTML = "<H1>INJECTED</H1>"
+"<H1>INJECTED</H1>"
+*/
