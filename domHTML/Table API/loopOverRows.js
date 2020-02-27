cce499d23927ecb982a4dc65d976ee51a0fb9987
projektorius96 <34185919+projektorius96@users.noreply.github.com>
2020-02-27 23:43:38 +0200

    Update loopOverRows.js

--- domHTML/Table API/loopOverRows.js
+++ domHTML/Table API/loopOverRows.js
@@ -1,13 +1,3 @@
-// gives result as matrix e.g 10x10
-for(var theRowToStartAt = 0 ; theRowToStartAt < 10; theRowToStartAt++) {
-tBODY.insertRow(-1)[theRowToStartAt];
-for(let numberOfCellsWithinEachRow = 0 ; numberOfCellsWithinEachRow < 11; numberOfCellsWithinEachRow++) {
-tBODY.rows[this.theRowToStartAt].insertCell(-1);
-}
-}
-
-/* */
-
 // gives result for 0 to 9 rows
 for(var i = 1 ; i < 10; i++) {
 tBODY.insertRow(-1)[i];
