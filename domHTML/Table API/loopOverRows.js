6f6bb08f1487d3c84e6b71e4bf755265b33a2dad
projektorius96 <34185919+projektorius96@users.noreply.github.com>
2020-02-27 23:32:29 +0200

    Update and rename loopOverForRows.js to loopOverRows.js

--- domHTML/Table API/loopOverRows.js
+++ domHTML/Table API/loopOverRows.js
@@ -1,7 +1,15 @@
+// gives result as matrix e.g 10x10
 for(var numberOfRows = 1 ; numberOfRows < 10; numberOfRows++) {
 tBODY.insertRow(-1)[numberOfRows];
 for(let numberOfCellsWithin = 0 ; numberOfCellsWithin < 9; numberOfCellsWithin++) {
 tBODY.rows[this.numberOfRows].insertCell(-1);
 }
 }
-// gives result as matrix e.g 10x10
+
+/* */
+
+// gives result for 0 to 9 rows
+for(var i = 1 ; i < 10; i++) {
+tBODY.insertRow(-1)[i];
+tBODY.rows[i].insertCell(-1);
+}
