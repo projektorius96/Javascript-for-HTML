39796a04851e35814644a6181cfe70d4580e6832
projektorius96 <34185919+projektorius96@users.noreply.github.com>
2020-04-17 21:07:12 +0300

    Update mostNeededDomProps.js

--- domHTML/domProperties/mostNeededDomProps.js
+++ domHTML/domProperties/mostNeededDomProps.js
@@ -1,3 +1,6 @@
 document.documentElement.parentNode.children // HTMLCollection [html]
 
 document.documentElement.parentNode.children[0].appendChild(newNode); // wheare as newNode is some user-custom created Node as HTMLElement
+// or insert HTML element not as node but as element straight away with .innerHTML prop:
+document.documentElement.parentNode.children[0].innerHTML += "<div>Hello world from div</div>"
+// when do check with var all = document.querySelectorAll("*"); which will give div as the last of paretNode within <body>
